703137ad676bc37d88d632a9108532badb041b6a
Mohit Raj <mohitraj3697@gmail.com>
2026-04-05 20:04:35 +0530

    added sim_engine wf

---
+++ langgraph_backend.py
@@ -0,0 +1,37 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(
+    model="openai/gpt-oss-20b",
+    temperature=0.5,
+    groq_api_key=groq_api_key
+)
+
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
